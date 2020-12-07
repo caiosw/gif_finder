e6caf538dd0c7caaa08929015d3629401cf68ef3
Caio Werneck <caiosw@gmail.com>
2020-12-07 00:08:46 -0300

    smooth transition

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:gif_finder/ui/gif_page.dart';
 import 'package:http/http.dart' as http;
 import 'package:share/share.dart';
-
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -111,7 +111,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   int _getCount(List data) {
-    if(_search == null) {
+    if(_search == null || _search.isEmpty) {
       return data.length;
     } else {
       return data.length + 1;
@@ -130,10 +130,11 @@ class _HomePageState extends State<HomePage> {
       itemBuilder: (context, index) {
         if (_search == null || index < snapshot.data["data"].length) {
           return GestureDetector(
-            child: Image.network(
-              snapshot.data["data"][index]["images"]["fixed_height"]["url"],
-              height: 300.0,
-              fit: BoxFit.cover,
+            child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                height: 300.0,
+                fit: BoxFit.cover,
             ),
             onTap: () {
               Navigator.push(
